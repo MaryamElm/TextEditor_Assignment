16bfefb414cefccc8ac187aa4e70b51d1af73f07
Maryam El Marjany <melma086@uottawa.ca>
2021-01-15 06:51:21 -0500

    Make the TextEditorGui reference the new class instead of Stack.java

--- assignment/part2/src/textEditor/gui/TextEditorGui.java
+++ assignment/part2/src/textEditor/gui/TextEditorGui.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Stack;
+import java.util.StackImplementation;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
@@ -35,7 +35,7 @@ import textEditor.util.PropertiesLoader;
  * @author Hussein Al Osman
  * @version 1.0
  * This is the main Graphical User Interface (GUI) class that defines the principal GUI components
- * and makes visible the main functions of the system 
+ * and makes visible the main functions of the system
  */
 public class TextEditorGui extends JFrame implements TextEventListener{
 
@@ -51,23 +51,23 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	 */
 	private TextParser textParser;
 	private SpellChecker spellChecker;
-	private Stack<TextEditEvent> stack;
+	private StackImplementation<TextEditEvent> stack;
 	private TextDocumentListener textDocumentListener;
-	
+
 	private EwlStructureAdaptor ewlStructure;
-	
+
 	/**
-	 * Text editor lone constructor 
-	 * 
+	 * Text editor lone constructor
+	 *
 	 */
 	public TextEditorGui(){
 		/**
 		 * TODO Change this default stack by providing your own implementation
 		 */
-		stack = new Stack<TextEditEvent>();
-		
+		stack = new StackImplementation<TextEditEvent>();
+
 		ewlStructure = new EwlListAdaptor();
-		
+
 		// Load the spellcheck EWL into memory
 		try {
 			ewlStructure.loadEwl();
@@ -75,11 +75,11 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 			// Display an error message and exit if EWL file cannot be loaded
 			JOptionPane.showMessageDialog(TextEditorGui.this, "Cannot load the EWL file",
 					"ERROR", JOptionPane.ERROR_MESSAGE);
-			
+
 			// Exit program since EWL file could not be loaded
 			exit(-1);
 		}
-		
+
 		// Initialize the GUI components and create their listeners
 		initGui();
 
@@ -91,7 +91,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 		// Set look and feel
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} 
+		}
 		catch (Exception e) {
 			// If this cannot be achieved, then do nothing, no harm is done
 		}
@@ -101,49 +101,49 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 		 */
 		// Create menu bar
 		JMenuBar menuBar = new JMenuBar();
-		
+
 		JMenu fileMenu = new JMenu("File");
 		JMenuItem openMenu = new JMenuItem("Open...");
 		JMenuItem exitMenu = new JMenuItem("Exit");
-		
+
 		JMenu editMenu = new JMenu("Edit");
 		JMenuItem spellCheckMenu = new JMenuItem("Spell check...");
 		JMenuItem undoMenu = new JMenuItem("Undo");
-		
+
 		JMenu helpMenu = new JMenu("Help");
 		JMenuItem aboutMenu = new JMenuItem("About");
-		
+
 		fileChooser = new JFileChooser();
 
 		menuBar.add(fileMenu);
 		fileMenu.add(openMenu);
 		fileMenu.add(exitMenu);
-		
+
 		menuBar.add(editMenu);
 		editMenu.add(spellCheckMenu);
 		editMenu.add(undoMenu);
-		
+
 		menuBar.add(helpMenu);
 		helpMenu.add(aboutMenu);
-		
+
 		setJMenuBar(menuBar);
-		
+
 		// Create text area
 		textArea = new JTextArea();
 		textArea.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "Undo");
 		textArea.getInputMap().put(KeyStroke.getKeyStroke("F7"), "SpellCheck");
-		JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+		JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		setContentPane(scrollPane);
-		
+
 		// Set some frame properties
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Text Editor");
 		this.setSize(800,600);
 		this.setResizable(true);
 		this.setVisible(true);
-		
-		
+
+
 		/**
 		 * All listeners are defined in this section
 		 */
@@ -160,14 +160,14 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 					performSpellCheck();
 			}
 		});
-		
+
 		textDocumentListener = new TextDocumentListener();
 		document = textArea.getDocument();
 		document.addDocumentListener(textDocumentListener);
 		textDocumentListener.addEventListener(this);
-		
-		
-		
+
+
+
 		// Open menu action listener
 		openMenu.addActionListener(new ActionListener(){
 			@Override
@@ -175,7 +175,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 				performOpenFile();
 			}
 		});
-		
+
 		// Exit menu action listener
 		exitMenu.addActionListener(new ActionListener(){
 			@Override
@@ -183,7 +183,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 				exit(0);
 			}
 		});
-		
+
 		// SpellCheck menu action listener
 		spellCheckMenu.addActionListener(new ActionListener(){
 			@Override
@@ -191,7 +191,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 				performSpellCheck();
 			}
 		});
-		
+
 		// Undo menu action listener
 		undoMenu.addActionListener(new ActionListener(){
 			@Override
@@ -199,16 +199,16 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 				performUndo();
 			}
 		});
-		
+
 		// About menu action listener
 		aboutMenu.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				showAbout();
 			}
-		});		
+		});
 	}
-	
+
 	/**
 	 * Launches the file chooser. If a file is chosen,
 	 * the file's text is read and set into the text area of the editor
@@ -224,9 +224,9 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 					JOptionPane.showMessageDialog(TextEditorGui.this, "Cannot read text file",
 							"ERROR", JOptionPane.ERROR_MESSAGE);
 				}
-	      }		
+	      }
 	}
-	
+
 	/**
 	 * Launches the spellcheck operation by retrieving the
 	 * text from the textArea, parsing is and running the spellChecking
@@ -235,17 +235,17 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	private void performSpellCheck(){
 		textParser = new TextParser(textArea.getText());
 		String[] textWords = textParser.parseText();
-		
+
 		spellChecker = new SpellChecker(textWords,ewlStructure);
-		
-		SpellCheckerGui spellCheckerGui = 
+
+		SpellCheckerGui spellCheckerGui =
 			new SpellCheckerGui(spellChecker.getIncorrectWords());
-		spellCheckerGui.setVisible(true);		
+		spellCheckerGui.setVisible(true);
 	}
-	
+
 	/**
 	 * Pops a previously stored text edit event form the
-	 * stack and reverses its effect. For instance, if the event is 
+	 * stack and reverses its effect. For instance, if the event is
 	 * and text INSERT_EVENT, the added string is removed. If it is
 	 * a REMOVE_EVENT, then the removed string is added back
 	 */
@@ -257,7 +257,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 				// programatically changing the content of the document so
 				// that such an event would not be mistaken to be a user action
 				textDocumentListener.setEnabled(false);
-				
+
 				if (editEvent.getEventType() == TextEditEvent.INSERT_EVENT){
 					// This was an insert event, therefore to undo it we should remove
 					document.remove(editEvent.getEditOffset(), editEvent.getEditLength());
@@ -266,17 +266,17 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 					// This is a remove event, thereofre to undo it we should insert
 					document.insertString(editEvent.getEditOffset(), editEvent.getEditStr(),null);
 				}
-				
+
 				// Turn the listener back on now that we have finished manipulating
 				// the document so that we can again catch the user actions
 				textDocumentListener.setEnabled(true);
-				
+
 			} catch (BadLocationException e1) {
 				e1.printStackTrace();
 			}
 		}
 	}
-			
+
 	/**
 	 * Shows information about the program
 	 */
@@ -284,15 +284,15 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 		try {
 			File aboutFile = PropertiesLoader.getAboutFile();
 			String aboutStr = readTextFile(aboutFile);
-			
+
 			JOptionPane.showMessageDialog(TextEditorGui.this, aboutStr,
 					"About", JOptionPane.INFORMATION_MESSAGE);
-			
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		}		
+		}
 	}
-	
+
 	/**
 	 * This method is an event handler. Whenever a user event is caught,
 	 * it is pushed into the stack so that it can be later reversed when
@@ -302,7 +302,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	public void newTextEvent(TextEditEvent e) {
 		stack.push(e);
 	}
-	
+
 	/**
 	 * Reads a file from the hard disk and retunrs its textual content
 	 * @param file to be read
@@ -311,17 +311,17 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	 */
 	private String readTextFile(File file) throws IOException{
 		StringBuffer textBuffer = new StringBuffer();
-		
+
 		FileReader fstream = new FileReader(file);
 		BufferedReader br = new BufferedReader(fstream);
-		
+
 		String line=null;
 		while ((line=br.readLine()) != null){
 			textBuffer.append(line+"\n");
 		}
-		
+
 		return textBuffer.toString();
-		
+
 	}
 
 	/**
@@ -331,7 +331,7 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	private void exit(int ret){
 		System.exit(ret);
 	}
-	
+
 	/**
 	 * Main method to launch the application
 	 * @param args
@@ -339,5 +339,5 @@ public class TextEditorGui extends JFrame implements TextEventListener{
 	public static void main (String [] args){
 		new TextEditorGui();
 	}
-	
+
 }
